84572cd90ace6f3afc5044ac3fdc9c29d3aa5c59
Iida Hiroki <minoru113@hotmail.com>
2021-10-23 22:54:33 +0900

    done 12.5

---
+++ src/lib.rs
@@ -0,0 +1,111 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+use std::env;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {query, filename, case_sensitive})
+    }
+}
+
+// memo: Box<Error> -> Box<dyn Error>
+// trait objects without an explicit `dyn` are deprecated
+// this was previously accepted by the compiler but is being phased out; it will become a hard error in the 2021 edition!
+// note: for more information, see issue #80165 <https://github.com/rust-lang/rust/issues/80165>
+pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+    
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+// Rust
+// 安全かつ高速で生産的
+// 三つを選んで
+// ガムテープ
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+// (最後の行のみ)
+// 私を信じて
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); 
@@ -14,10 +16,13 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.filename);
 
-    run(config);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error! {}", e);
+        process::exit(1);
+    }
 
     // let mut f = File::open(config.filename).expect("file not found");
 
@@ -26,34 +31,6 @@ fn main() {
     //     .expect("something went wrong reading the file");
 
     // println!("With text:\n{}", contents);
-}
-
-fn run (config: Config) {
-    let mut f = File::open(config.filename).expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {query, filename})
-    }
 }
 
 // fn parse_config(args: &[String]) -> Config {
